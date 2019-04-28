bf8cf2682871b19e35f2f58bf50691b094386927
chernovrv <sanbonzakura5@gmail.com>
2019-04-28 20:46:26 +0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int number = 4;
+        double a = 4.5;
+        byte b = 1;
+        short c = 2;
+        long d = 3;
+        float e = 5;
+        char f = 6;
+        boolean g = false;
+        String a1 = "Привет Пока";
+        String a2 = "Do";
+        String a3 = "ta";
+        String a4 = a2+a3;
+        int h = 1992; String h1 = "Roma";
+        String h2 = h+h1;
+        System.out.println( number+""+ a+""+ b+""+c+""+d+""+e+""+f+""+g+""+a1+""+a4+""+h2);
+
+    }
+};
+
