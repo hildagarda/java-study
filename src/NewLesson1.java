fd7f44b7b7fb021f8bf4c383c7a3115653748c52
chernovrv <sanbonzakura5@gmail.com>
2019-10-18 18:23:01 +0500

    add: task5,task6,task7 - loop

---
+++ src/NewLesson1.java
@@ -0,0 +1,14 @@
+public class NewLesson1 {
+    public static void main(String[] args) {
+
+        int a = 1;
+        char b = '$';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+" "+b);
+
+        System.out.println("Hello world");
+    }
+
+}
